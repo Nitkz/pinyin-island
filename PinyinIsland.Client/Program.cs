c9e2ac5fd5ea9c5e49c0165e21227f414761b63d
Nitikorn <nitkz@hotmail.com>
2026-09-16 04:20:58 +0700

    fix(client): resolve '#app' selector error during interactive WebAssembly bootstrap
    - Remove unnecessary RootComponents registration from PinyinIsland.Client/Program.cs
    - Remove redundant client index.html to align with Blazor Web App server-rendered App.razor structure

--- PinyinIsland.Client/Program.cs
+++ PinyinIsland.Client/Program.cs
@@ -1,12 +1,8 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-using PinyinIsland.Client;
 using PinyinIsland.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<Routes>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
@@ -14,4 +10,3 @@ builder.Services.AddScoped<IProgressService, ProgressService>();
 builder.Services.AddScoped<IStageDataService, StageDataService>();
 
 await builder.Build().RunAsync();
-
